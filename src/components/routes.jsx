e95e2208f6dd44ca3d4836ccc50710f4b522f4df
barakacanbyte <barakajimmy@outlook.com>
2024-08-05 14:58:57 +0300

    blank screen solved

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -9,39 +9,39 @@ import MainShop from "./mainShop.jsx";
 
 const routes = [
   {
-    path: '/',
-    element: <homepage />,
+    path: "/gameShop-app-react/",
+    element: <Homepage />,
     errorElement: <ErrorPage />,
   },
   {
-    path: 'shop',
+    path: "shop",
     element: <Shop />,
     errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <Navigate to='games' />
+        element: <Navigate to="games" />,
       },
       {
-        path: 'games',
+        path: "games",
         element: <MainShop />,
         children: [
           {
             index: true,
-            element: <Navigate to='p' />,
+            element: <Navigate to="p" />,
           },
           {
-            path: ':gamesId',
-            element: <Games />
-          }
-        ]
+            path: ":gamesId",
+            element: <Games />,
+          },
+        ],
       },
       {
-        path: 'game/:gameId',
-        element: <Game />
-      }
-    ]
-  }
+        path: "game/:gameId",
+        element: <Game />,
+      },
+    ],
+  },
 ];
 
 const router = createBrowserRouter(routes, {
